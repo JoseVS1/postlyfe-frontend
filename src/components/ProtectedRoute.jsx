55bd660407b830da0369ba353e7cb05e3b6a4396
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2025-04-21 17:27:57 -0600

    Style loading message

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,9 @@ import { Navigate } from "react-router";
 
 export const ProtectedRoute = ({ user, loading, children }) => {
     if (loading) {
-      return <div>Loading...</div>;
+      return <div className="loading-container">
+        <h2>Loading...</h2>
+      </div>;
     }
 
     if (!user) {
